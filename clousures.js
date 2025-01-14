3166fd120f820218113e6f0e188a29c69e9cefb0
HIMANI <himani.tomar@evbox.com>
2025-01-14 18:19:25 +0100

    Add more details to closures with exaple and explination

--- clousures.js
+++ clousures.js
@@ -1,15 +1,40 @@
-function x(){
-    var a = 9;
 
-    return function y(){
-        console.log(a);
+// Closures is a combination of function  bundelled together with refernces to its surrounding state. 
+//OR closures gives function access to its Outer Scope
+
+
+function closuresExample(){
+    var nameOfCity = "Eindhoven";
+    function displayNameOfTheCity(){
+        console.log(`Name of the city is ${nameOfCity}`);
+        console.log(nameOfCity);
     }
-var a =10;
-   
+    displayNameOfTheCity();
+}
+closuresExample();
+
+
+/*
+ * BeforeES6 var has ----> Function-scoped or Global Scoped.
+ * Blocks with curly braches {} do not create scope
+ * 
+ * 
+ * */
+
+
+if(Math.random()>0.6){
+    var x= 4;
+}else {
+    var x =5 ;
 }
 
-var z = x();
-console.log(z);
+console.log(x);
 
-z();
+/**
+ * The above should throw error but doesnt because x is treated as global variable. 
+ * If we change the type to const or let we will get Reference error x is not defined.
+ * This is because const and let are blocked scope
+ */
+
+
 
